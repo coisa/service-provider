b9b1fc9f1f9fe51d81c5b628b7cde97c00c327bb
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-10-03 21:14:34 -0300

    Remove warnings from tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of coisa/service-provider.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/service-provider
+ * @copyright Copyright (c) 2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$errorReporting = \error_reporting();
+
+// Removing warnings on tests (just because coverage report are flooding the warnings)
+\error_reporting($errorReporting ^ E_WARNING);
